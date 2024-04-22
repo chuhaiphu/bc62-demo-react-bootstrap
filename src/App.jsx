534b9ef73a52065ea2db72bcf75dee3b7f4980ad
doanvinhphu <phudvse171839@fpt.edu.vn>
2024-04-22 21:43:59 +0700

    update code

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./demo/navbar"
+import Header from "./demo/header"
+import Section from "./demo/section"
+import Footer from "./demo/footer"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Section />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
